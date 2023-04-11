d5c916885940c9b5032ee220a1d0c7308a099ceb
donghaoliu <1473551776@qq.com>
2023-04-12 02:31:34 +0800

    feat: redis存储文件分块检查已经上传的文件分块

--- db/redis.go
+++ db/redis.go
@@ -6,7 +6,7 @@ import (
 )
 
 // GetRedis 连接redis
-func GetRedis() *redis.Client {
+func initRedis() *redis.Client {
 
 	redisConfig := config.Configs.Redis
 	client := redis.NewClient(&redis.Options{
@@ -21,11 +21,14 @@ var redisDB *redis.Client
 
 func init() {
 	// 初始化redis连接
-	redisDB = GetRedis()
+	redisDB = initRedis()
 	//最大连接数
 	redisDB.Options().MaxRetries = 100
 	//最大空闲连接数
 	redisDB.Options().MaxRetries = 10
-	//连接超时时间
-	redisDB.Options().DialTimeout = 100
+
+}
+
+func GetRedis() *redis.Client {
+	return redisDB
 }
--- handler/upload/upload.go
+++ handler/upload/upload.go
@@ -1,13 +1,16 @@
 package upload
 
 import (
+	"context"
 	"fmt"
 	"github.com/gin-gonic/gin"
 	"github.com/gin-gonic/gin/binding"
+	"github.com/rujiefenfang/filestore-server/db"
 	"github.com/rujiefenfang/filestore-server/model"
 	"net/http"
 	"os"
 	"strconv"
+	"time"
 )
 
 // View 上传界面
@@ -24,7 +27,6 @@ func View(c *gin.Context) {
 func GetUploadUrl(c *gin.Context) {
 	fileName := c.Query("fileName")
 	chunkCount, _ := strconv.Atoi(c.Query("chunkCount"))
-
 	fmt.Println("Getting upload URL for file:", fileName)
 
 	// 创建目录
@@ -52,15 +54,46 @@ func Chunks(c *gin.Context) {
 	valueMap := form.Value
 	// 获取文件块数
 	chunkIndex := valueMap["chunkIndex"][0]
+	// 获取sha1
+	fileSha1 := valueMap["fileSha1"][0]
 
-	fmt.Println("Uploading chunk", chunkIndex, "of file", fileName)
-
-	// 保存切片
-	_, header, _ := c.Request.FormFile("file")
-	err := c.SaveUploadedFile(header, "./uploads/"+fileName+"/"+chunkIndex)
+	// 获取文件
+	_, header, err := c.Request.FormFile("file")
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{
 			"error": "Failed to save chunk " + chunkIndex + " of file " + fileName,
+		})
+		return
+	}
+
+	// 保存文件
+	err = c.SaveUploadedFile(header, "./uploads/"+fileName+"/"+chunkIndex)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"error": "Failed to save chunk " + chunkIndex + " of file " + fileName,
+		})
+		return
+	}
+
+	ctx := context.Background()
+	redis := db.GetRedis()
+
+	// 将文件块的索引存入redis
+	err = redis.RPush(ctx, fileSha1, chunkIndex).Err()
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"status": "Failed",
+			"error":  err.Error(),
+		})
+		return
+	}
+
+	// 设置过期时间为1天
+	err = redis.Expire(ctx, fileSha1, time.Hour*24).Err()
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"status": "Failed",
+			"error":  err.Error(),
 		})
 		return
 	}
@@ -70,8 +103,11 @@ func Chunks(c *gin.Context) {
 	})
 }
 
-// CheckFile 检查文件是否已经上传
+// CheckFile 检查文件是否已经上传完成
 func CheckFile(c *gin.Context) {
+
+	var finishedChunks []string
+	var isExist bool
 
 	checkFile := model.CheckFile{}
 	// 检查请求参数
@@ -83,15 +119,34 @@ func CheckFile(c *gin.Context) {
 		})
 		return
 	}
+	ctx := context.Background()
+	// 获取redis客户端
+	redis := db.GetRedis()
+	redis.Set(ctx, "test", "test", 0)
+	redis.LPush(ctx, "test1", "")
+	fmt.Println(redis)
+	// 根据文件的sha1值，从数据库中查询记录,如果不存在者设置key=fileSha1，value=[]，并设置过期时间为1天
+	mGet := redis.LRange(ctx, checkFile.FileSha1, 0, -1)
+	// 如果存在
+	if len(mGet.Val()) != 0 {
+		// 获取已经上传的切片
+		finishedChunks = mGet.Val()
+		isExist = true
+		// 返回已经上传的切片
+		c.JSON(http.StatusOK, gin.H{
+			"status":         "OK",
+			"isExist":        isExist,
+			"finishedChunks": finishedChunks,
+		})
+		return
+	} else {
+		// 如果不存在
+		c.JSON(http.StatusOK, gin.H{
+			"status":         "OK",
+			"isExist":        isExist,
+			"finishedChunks": finishedChunks,
+		})
 
-	//TODO: 检查文件是否已经上传
-	//根据文件的sha1值，从数据库中查询记录
-	var finishedChunks []string
-	var isExist bool
+	}
 
-	c.JSON(http.StatusOK, gin.H{
-		"status":         "OK",
-		"isExist":        isExist,
-		"finishedChunks": finishedChunks,
-	})
 }
